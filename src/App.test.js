a7ad08b3c2f72f4b6f248c12f78422bcd9ad591e
Chiyen Venegas <chi@marandino.dev>
2023-05-15 11:34:16 -0600

    ♻️  implements a `describe` test block for `app.js`
    improves the code readability

--- src/App.test.js
+++ src/App.test.js
@@ -1,25 +1,27 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
 
-it("renders the title, and main button", () => {
-  render(<App />);
-  screen.getByText(/Random Command/i);
-  screen.getByRole("button");
-});
+describe("Main page tests", () => {
+  it("renders the title, and main button", () => {
+    render(<App />);
+    screen.getByText(/Random Command/i);
+    screen.getByRole("button");
+  });
 
-//TODO: refactor this into SFC, and migrate the test to individual component unit tests.
-test("clicking the button generates a new command", async () => {
-  render(<App />);
-  // check if the button exists.
-  const generateButton = screen.getByRole("button", { name: /generate/i });
-  // get the subheader, the element with the "command"
-  const subHeaderElement = screen.getByTestId("randomCommand");
-  // store the value before the click
-  const subHeaderBeforeClick = subHeaderElement.textContent;
-  //click
-  await fireEvent.click(generateButton);
-  //store the value after the click
-  const subHeaderAfterClick = subHeaderElement.textContent;
-  //assert
-  expect(subHeaderBeforeClick).not.toMatch(subHeaderAfterClick);
+  //TODO: refactor this into SFC, and migrate the test to individual component unit tests.
+  it("generates a new command after clicking the primary button", async () => {
+    render(<App />);
+    // check if the button exists.
+    const generateButton = screen.getByRole("button", { name: /generate/i });
+    // get the subheader, the element with the "command"
+    const subHeaderElement = screen.getByTestId("randomCommand");
+    // store the value before the click
+    const subHeaderBeforeClick = subHeaderElement.textContent;
+    //click
+    await fireEvent.click(generateButton);
+    //store the value after the click
+    const subHeaderAfterClick = subHeaderElement.textContent;
+    //assert
+    expect(subHeaderBeforeClick).not.toMatch(subHeaderAfterClick);
+  });
 });
